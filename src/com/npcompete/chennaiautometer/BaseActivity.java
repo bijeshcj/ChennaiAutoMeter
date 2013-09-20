e37a2ac41861622fb4cf5b983bb0acf0e9a9b625
NPCompete <npcompete@npcompete-desktop.(none)>
2013-09-20 19:21:11 +0530

    finding whether the device is not moving

--- src/com/npcompete/chennaiautometer/BaseActivity.java
+++ src/com/npcompete/chennaiautometer/BaseActivity.java
@@ -2,6 +2,7 @@ package com.npcompete.chennaiautometer;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class BaseActivity extends Activity {
@@ -10,6 +11,8 @@ public class BaseActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_base);
+		startService(new Intent(this, BaseService.class));
+		
 	}
 
 	@Override
